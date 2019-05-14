60552221727b98fee32928cf8f17fbb5b36ec7b4
Junior Gantin <nioperas06@gmail.com>
2019-05-14 11:02:42 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Header from './layout/Header'
+import Footer from './layout/Footer'
+
+import Home from './components/Home'
+import Login from './components/Login'
+import About from './components/About'
+import Product from './components/Product'
+import NotFound from './components/NotFound'
+import Collection from './components/Collection'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/login" component={Login} />
+          <Route path="/products/:product" component={Product} />
+          <PrivateRoute authed={false} path='/collections' component={Collection} />
+          <Route component={ NotFound } />
+        </Switch>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
